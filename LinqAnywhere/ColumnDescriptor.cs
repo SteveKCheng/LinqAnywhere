0871998e572b0013806e91473f8153d7a0d1fef0
Steve Cheng <steve.ckp@gmail.com>
2018-10-21 21:17:31 -0400

    Better code documentation and minor code adjustments

--- LinqAnywhere/ColumnDescriptor.cs
+++ LinqAnywhere/ColumnDescriptor.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Linq.Expressions;
 
 namespace LinqAnywhere
@@ -5,12 +6,30 @@ namespace LinqAnywhere
     /// <summary>
     /// Describes an indexed column in an AbstractTable.
     /// </summary>
-    public struct ColumnDescriptor
+    /// <remarks>
+    /// <para>
+    /// For the purposes of this library, a "column" even without
+    /// the qualification "indexable", is assumed to be part of a table index.
+    /// An abstract table does not need an explicit representation of 
+    /// any other kind of column, because without an index, there is nothing
+    /// that a relational query processor can do to optimize accesses to it.
+    /// This design is deliberate so that, unlike SQL, some parts of the data 
+    /// in the table row can be in any format (e.g. it could be an array of items, 
+    /// something that SQL handles terribly) if relational queries on that 
+    /// part of data are not needed.  In other words, non-indexable data
+    /// is just treated as a blob.
+    /// </para>
+    /// <para>
+    /// This class holds the most essential data for querying on a
+    /// column as part of an index, for optimizing relational queries.
+    /// </para>
+    /// </remarks>
+    public class ColumnDescriptor
     {
         /// <summary>
         /// Whether the values of the column have a total ordering.
         /// </summary>
-        public bool IsOrdered { get; }
+        public bool IsOrdered => TotalOrder != null;
 
         /// <summary>
         /// Whether multiple rows in the same table can share the
@@ -18,12 +37,34 @@ namespace LinqAnywhere
         /// </summary>
         public bool IsUnique { get; }
 
-        public ColumnDescriptor(Expression columnExpression)
+        /// <summary>
+        /// Construct descriptor for a column with total ordering of its values.
+        /// </summary>
+        public ColumnDescriptor(Expression rowExpression, 
+                                Expression columnExpression, 
+                                IComparer totalOrder,
+                                bool isUnique)
         {
+            this.RowExpression = rowExpression;
             this.ColumnExpression = columnExpression;
-            this.IsOrdered = true;
-            this.IsUnique = false;
-            this.RowExpression = null;
+            this.TotalOrder = totalOrder;
+            this.Equivalence = null;
+            this.IsUnique = isUnique;
+        }
+
+        /// <summary>
+        /// Construct descriptor for a column without total ordering of its values.
+        /// </summary>
+        public ColumnDescriptor(Expression rowExpression,
+                                Expression columnExpression, 
+                                IEqualityComparer equivalence,
+                                bool isUnique)
+        {
+            this.RowExpression = rowExpression;
+            this.ColumnExpression = columnExpression;
+            this.TotalOrder = null;
+            this.Equivalence = equivalence;
+            this.IsUnique = isUnique;
         }
 
         /// <summary>
@@ -39,6 +80,42 @@ namespace LinqAnywhere
         /// </summary>
         public Expression RowExpression { get; }
 
+        /// <summary>
+        /// If the values of the column are totally ordered, 
+        /// this member gives the IComparer implementation to give
+        /// the relative positioning of two values.
+        /// </summary>
+        public IComparer TotalOrder { get; } 
+
+        /// <summary>
+        /// If the values do not have a total ordering,
+        /// this members gives the IEqualityComparer implementation to
+        /// compare if two values are equal.
+        /// </summary>
+        public IEqualityComparer Equivalence { get; }
+
+        /// <summary>
+        /// Determine whether a LINQ expression evaluates to this column
+        /// for a row in the abstract table.
+        /// </summary>
+        /// <remarks>
+        /// <para>
+        /// As a concrete example, if each row R of the abstract table,
+        /// as a .NET object, has a property X that maps to this column,
+        /// then this method returns true for the LINQ expression R.X 
+        /// and false for any other expression.
+        /// </para>
+        /// <para>
+        /// This method is used to match conditional clauses in a LINQ query
+        /// to the indices on an abstract table that can be used to optimize
+        /// the query.
+        /// </para>
+        /// </remarks>
+        /// <param name="subject">Expression referring to an arbitrary row of the 
+        /// abstract table. </param>
+        /// <param name="expr">The LINQ expression to test. </param>
+        /// <returns>True if the LINQ expression exactly matches this column,
+        /// otherwise false. </returns>
         public bool MatchesExpression(ParameterExpression subject, Expression expr)
         {
             // FIXME subject needs to be considered in the comparison
--- LinqAnywhere/TableIndex.cs
+++ LinqAnywhere/TableIndex.cs
@@ -5,6 +5,32 @@ namespace LinqAnywhere
     /// <summary>
     /// Describes a primary or secondary index in an AbstractTable.
     /// </summary>
+    /// <remarks>
+    /// <para>
+    /// An index for an abstract table is essentially a sequence of
+    /// columns that have been "indexed", i.e. searches for values
+    /// over the columns are efficient.  
+    /// </para>
+    /// <para>
+    /// The computational model for an index as defined here has been 
+    /// abstracted from the usual way they are implemented in SQL databases: 
+    /// the values of the columns, written in the order the columns appear, 
+    /// form a sort key is stored in sorted order in a one-dimensional array
+    /// associated with the data table.  Thus a multi-dimensional key is flattened 
+    /// into a single-dimensional key.  The values in the indexable columns
+    /// are assumed to have a total order, and the flattened key has the induced
+    /// total ordering.  With this total ordering, the rows corresponding to 
+    /// any prefix sub-sequence of values for the indexable columns can be
+    /// efficiently found with binary search.
+    /// </para>
+    /// <para>
+    /// SQL databases may also implement indices as hash tables, which do not
+    /// require total ordering of the individual columns, and do not provide a
+    /// total ordering on the sequence of columns.  Hash tables
+    /// do not allow ranged queries or sub-key queries: only the full tuple
+    /// of column values may be efficiently sought for. 
+    /// </para>
+    /// </remarks>
     public struct TableIndex
     {
         /// <summary>
@@ -21,6 +47,10 @@ namespace LinqAnywhere
         /// Whether this table index is ordered, i.e. supports
         /// sequential iteration in order.
         /// </summary>
+        /// <remarks>
+        /// If true, the index is probably backed by some kind of sorted
+        /// array.   If false, the table is probably backed by a hash table.
+        /// </remarks>
         public bool IsOrdered { get; }
 
         /// <summary>
@@ -43,7 +73,7 @@ namespace LinqAnywhere
             => columns[columnOrdinal];
 
         /// <summary>
-        /// Attempt to match each term (an expression) to a column of a table index.
+        /// Attempt to match each term (in a LINQ expression) to a column of a table index.
         /// </summary>
         /// <param name="subject">Expression referring to a row of the table, used
         /// inside each term. </param>
@@ -55,7 +85,7 @@ namespace LinqAnywhere
         {
             var matchInfo = new IndexColumnMatch[terms.Length];
 
-            for(int i = 0; i < terms.Length; ++i)
+            for (int i = 0; i < terms.Length; ++i)
             {
                 var term = terms[i];
 
